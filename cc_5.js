a998f474a2153c61e007d55723456c3ab5a83519
Vanessa Do <xex.vanessa@gmail.com>
2025-02-05 23:59:53 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -10,3 +10,16 @@ for (let key in customer){
     if (customer.hasOwnProperty(key))
     console.log(`${key}: ${customer[key]}`)
 }
+
+// Task 2: Object Methods 
+//Declared order 
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder () { // used displayOrder method to display information individually 
+        console.log(`OrderID: ${this.orderId}`);
+        console.log(`Total Amount: $${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }};
+order.displayOrder(); // logs the orderId, totalAmount, and status 
